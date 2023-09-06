214f9d2719d1c549032a4356ff22ac9ebef810a8
Soul_KRT <soul.krt.00@gmail.com>
2023-09-06 16:43:28 +0300

    fix last commit

--- YamlConfig/src/main/java/tkachgeek/config/base/Utils.java
+++ YamlConfig/src/main/java/tkachgeek/config/base/Utils.java
@@ -2,6 +2,7 @@ package tkachgeek.config.base;
 
 
 import java.io.IOException;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -9,33 +10,35 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
 public class Utils {
-  public static String readString(Path path) {
-    try {
-      return Files.readString(path, StandardCharsets.UTF_8);
-    } catch (IOException ignored) {
-    }
-    return "";
-  }
-  
-  public static void writeString(Path path, String text) {
-    try {
-      if(!Files.exists(path)) {
-        com.google.common.io.Files.createParentDirs(path.toFile());
+   public static String readString(Path path) {
+      try {
+         return Files.readString(path, StandardCharsets.UTF_8);
+      } catch (IOException ignored) {
       }
-      
-      Files.writeString(path, text, StandardCharsets.UTF_8, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-  
-  public static <T extends Config> T getNewInstance(Class<T> type) {
-    try {
-      return type.getDeclaredConstructor().newInstance();
-    } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-      e.printStackTrace();
-    }
-    return null;
-  }
-  
+      return "";
+   }
+
+   public static void writeString(Path path, String text) {
+      try {
+         if (!Files.exists(path)) {
+            com.google.common.io.Files.createParentDirs(path.toFile());
+         }
+
+         Files.writeString(path, text, StandardCharsets.UTF_8, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
+
+   public static <T extends Config> T getNewInstance(Class<T> type) {
+      try {
+         Constructor<T> declaredConstructor = type.getDeclaredConstructor();
+         declaredConstructor.setAccessible(true);
+         return declaredConstructor.newInstance();
+      } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+         e.printStackTrace();
+      }
+      return null;
+   }
+
 }
